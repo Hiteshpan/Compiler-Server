13f5104318cad55732812257b676d0bc81ceec42
Hiteshpan <hiteshrtk999@gmail.com>
2024-08-23 15:06:23 +0530

    initial commit

---
+++ dist/routes/userRouter.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userRouter = void 0;
+const userController_1 = require("../controllers/userController");
+const verifyToken_1 = require("../middlewares/verifyToken");
+const compilerController_1 = require("../controllers/compilerController");
+const express = require("express");
+exports.userRouter = express.Router();
+exports.userRouter.post("/signup", userController_1.signup);
+exports.userRouter.post("/login", userController_1.login);
+exports.userRouter.post("/logout", userController_1.logout);
+exports.userRouter.get("/user-details", verifyToken_1.verifyToken, userController_1.userDetails);
+exports.userRouter.get("/my-codes", verifyToken_1.verifyToken, compilerController_1.getMyCodes);
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { dbConnect } from "./lib/dbConnect";
+import { compilerRouter } from "./routes/compilerRouter";
+import { userRouter } from "./routes/userRouter";
+import cookieParser from "cookie-parser";
+
+const app = express();
+config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    credentials: true,
+    origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+}));
+// console.log("is your client url:", process.env.CLIENT_URL)
+
+app.use("/compiler", compilerRouter);
+app.use("/user", userRouter);
+
+dbConnect();
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+})
