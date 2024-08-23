83abb62ba70a0252e286b00913e02ca63ef154c6
Hitesh Kumar <145363832+Hiteshpan@users.noreply.github.com>
2024-08-24 00:08:02 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,32 @@ import { dbConnect } from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compilerRouter";
 import { userRouter } from "./routes/userRouter";
 import cookieParser from "cookie-parser";
+
+// Load environment variables
+config();
+
 const app = express();
 const port = parseInt(process.env.PORT || "3000", 10);
 
+// Middleware setup
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+        origin: [process.env.CLIENT_URL], // Use the CLIENT_URL from environment
         methods: ['POST', 'GET'],
         credentials: true,
     })
 );
-config();
 
+// Route handlers
 app.use("/compiler", compilerRouter);
 app.use("/user", userRouter);
 
+// Connect to the database
 dbConnect();
+
+// Start the server
 app.listen(port, '0.0.0.0', () => {
-    console.log(`App listening on port ${port}`)
-})
+    console.log(`App listening on port ${port}`);
+});
