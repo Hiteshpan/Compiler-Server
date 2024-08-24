43231308a2c883502a00191cf6e2bd5d30793947
Hiteshpan <hiteshrtk999@gmail.com>
2024-08-24 13:21:47 +0530

    testing

--- dist/index.js
+++ dist/index.js
@@ -10,18 +10,24 @@ const dbConnect_1 = require("./lib/dbConnect");
 const compilerRouter_1 = require("./routes/compilerRouter");
 const userRouter_1 = require("./routes/userRouter");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const app = (0, express_1.default)();
 (0, dotenv_1.config)();
+const app = (0, express_1.default)();
+// const port = parseInt(process.env.PORT || "3000", 10);
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
+    // origin: ["http://localhost:5173", process.env.CLIENT_URL!],
+    origin: ["https://compiler-client.vercel.app/", process.env.CLIENT_URL],
+    methods: ['POST', 'GET'],
     credentials: true,
-    origin: process.env.CLIENT_URL,
 }));
-// console.log("is your client url:", process.env.CLIENT_URL)
 app.use("/compiler", compilerRouter_1.compilerRouter);
 app.use("/user", userRouter_1.userRouter);
 (0, dbConnect_1.dbConnect)();
-app.listen(4000, () => {
-    console.log("http://localhost:4000");
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`App listening on port ${port}`)
+// })
+const port = parseInt(process.env.PORT_URL || "3000", 10);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Example app listening on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,45 @@ import { dbConnect } from "./lib/dbConnect";
 import { compilerRouter } from "./routes/compilerRouter";
 import { userRouter } from "./routes/userRouter";
 import cookieParser from "cookie-parser";
+config();
+
 const app = express();
-const port = parseInt(process.env.PORT || "3000", 10);
+// const port = parseInt(process.env.PORT || "3000", 10);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
+        origin: [process.env.CLIENT_URL!, "http://localhost:5174"],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         credentials: true,
-        origin: ["http://localhost:5173", process.env.CLIENT_URL!],
     })
 );
-config();
+
+// Explicitly handling preflight requests
+app.options('*', cors({
+    origin: [process.env.CLIENT_URL!],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}));
+
+app.use((req, res, next) => {
+    res.on('finish', () => {
+        console.log('Access-Control-Allow-Origin:', res.getHeader('Access-Control-Allow-Origin'));
+    });
+    next();
+});
 
 app.use("/compiler", compilerRouter);
 app.use("/user", userRouter);
 
 dbConnect();
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`App listening on port ${port}`)
+// })
+
+const port: number = parseInt(process.env.PORT_URL || "3000", 10);
+
 app.listen(port, '0.0.0.0', () => {
-    console.log(`App listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
